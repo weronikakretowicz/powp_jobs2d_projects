7ba05ebbebc761f03c9334b783c58c259be96531
NorbertMarcinkowski <norbert.marcinkowski@protonmail.com>
2024-04-11 10:30:14 +0200

    fix the method toString and format code

--- src/main/java/edu/kis/powp/jobs2d/drivers/DriversComposite.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/DriversComposite.java
@@ -4,46 +4,45 @@ import edu.kis.powp.jobs2d.Job2dDriver;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class DriversComposite implements Job2dDriver {
 
-	private List<Job2dDriver> list;
+    private List<Job2dDriver> list;
 
-	public DriversComposite() {
-		this.list = new ArrayList<>();
-	}
+    public DriversComposite() {
+        this.list = new ArrayList<>();
+    }
 
-	public DriversComposite(List<Job2dDriver> list) {
-		this.list = list;
-	}
+    public DriversComposite(List<Job2dDriver> list) {
+        this.list = list;
+    }
 
-	public void addDriver(Job2dDriver driver) {
-		this.list.add(driver);
-	}
+    public void addDriver(Job2dDriver driver) {
+        this.list.add(driver);
+    }
 
-	public boolean removeDriver(Job2dDriver driver) {
-		return list.remove(driver);
-	}
+    public boolean removeDriver(Job2dDriver driver) {
+        return list.remove(driver);
+    }
 
-	@Override
-	public void setPosition(int x, int y) {
-		for (Job2dDriver driver : list) {
-			driver.setPosition(x, y);
-		}
-	}
+    @Override
+    public void setPosition(int x, int y) {
+        for (Job2dDriver driver : list) {
+            driver.setPosition(x, y);
+        }
+    }
 
-	@Override
-	public void operateTo(int x, int y) {
-		for (Job2dDriver driver : list) {
-			driver.operateTo(x, y);
-		}
-	}
+    @Override
+    public void operateTo(int x, int y) {
+        for (Job2dDriver driver : list) {
+            driver.operateTo(x, y);
+        }
+    }
 
-	public String toString() {
-		StringBuilder stringBuilder = new StringBuilder();
-		for (Job2dDriver driver : list) {
-			stringBuilder.append(driver.toString()).append("\n");
-		}
-		return stringBuilder.toString();
-	}
+    public String toString() {
+        return list.stream()
+                .map(Job2dDriver::toString)
+                .collect(Collectors.joining("\n", "Composite of ", ""));
+    }
 }
