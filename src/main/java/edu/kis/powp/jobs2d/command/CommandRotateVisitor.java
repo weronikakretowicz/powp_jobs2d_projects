4caccc9c3dabd448fabef9f06ce60f86240f670d
mwteusz <240759@edu.p.lodz.pl>
2024-04-21 01:06:57 +0200

    add rotation visitor

---
+++ src/main/java/edu/kis/powp/jobs2d/command/CommandRotateVisitor.java
@@ -0,0 +1,37 @@
+package edu.kis.powp.jobs2d.command;
+
+import edu.kis.powp.jobs2d.command.manager.TransformationVisitor;
+
+public class CommandRotateVisitor extends TransformationVisitor {
+
+    private final double angleInRadians;
+
+    public CommandRotateVisitor(double degrees) {
+        super("Rotated(" + degrees + "°)");
+        this.angleInRadians = Math.toRadians(degrees);
+    }
+
+    @Override
+    public void visit(OperateToCommand operateToCommand) {
+        operateToCommand.execute(getDriver());
+        int x = getDriver().getX();
+        int y = getDriver().getY();
+        int newX = (int) (x * Math.cos(this.angleInRadians) - y * Math.sin(this.angleInRadians));
+        int newY = (int) (x * Math.sin(this.angleInRadians) + y * Math.cos(this.angleInRadians));
+
+        DriverCommand scaledCommand = new OperateToCommand(newX, newY);
+        this.add(scaledCommand);
+    }
+
+    @Override
+    public void visit(SetPositionCommand setPositionCommand) {
+        setPositionCommand.execute(getDriver());
+        int x = getDriver().getX();
+        int y = getDriver().getY();
+        int newX = (int) (x * Math.cos(this.angleInRadians) - y * Math.sin(this.angleInRadians));
+        int newY = (int) (x * Math.sin(this.angleInRadians) + y * Math.cos(this.angleInRadians));
+
+        DriverCommand scaledCommand = new SetPositionCommand(newX, newY);
+        this.add(scaledCommand);
+    }
+}
