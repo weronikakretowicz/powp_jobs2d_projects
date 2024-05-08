24bb49b8d8bf37d9c19cb0e768d6839e13afccc5
Jakub Glinka <jakubglinka9@gmail.com>
2024-05-08 17:00:59 +0200

    Add set of static factory methods to FlippingDriverDecorator

--- src/main/java/edu/kis/powp/jobs2d/drivers/transformators/FlippingDriverDecorator.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/transformators/FlippingDriverDecorator.java
@@ -2,7 +2,7 @@ package edu.kis.powp.jobs2d.drivers.transformators;
 
 import edu.kis.powp.jobs2d.Job2dDriver;
 
-public class FlippingDecorator extends TransformationsDriverDecorator {
+public class FlippingDriverDecorator extends Job2dDriverDecorator {
     public static final int FLIP_VERTICALLY = 0;
     public static final int FLIP_HORIZONTALLY = 1;
     public static final int FLIP_BOTH_SIDES = 2;
@@ -10,7 +10,7 @@ public class FlippingDecorator extends TransformationsDriverDecorator {
     private int flippedX = 0, flippedY = 0;
     private int flippingDirection;
 
-    public FlippingDecorator(Job2dDriver job2dDriver, int flippingDirection) {
+    private FlippingDriverDecorator(Job2dDriver job2dDriver, int flippingDirection) {
         super(job2dDriver);
         this.flippingDirection = flippingDirection;
     }
@@ -49,11 +49,15 @@ public class FlippingDecorator extends TransformationsDriverDecorator {
         }
     }
 
-    public void setFlippingDirection(int flippingDirection) {
-        this.flippingDirection = flippingDirection;
+    static public FlippingDriverDecorator getFlipHorizontalDecorator(Job2dDriver job2dDriver) {
+        return new FlippingDriverDecorator(job2dDriver, FLIP_HORIZONTALLY);
     }
 
-    public int getFlippingDirection() {
-        return flippingDirection;
+    static public FlippingDriverDecorator getFlipVerticalDecorator(Job2dDriver job2dDriver) {
+        return new FlippingDriverDecorator(job2dDriver, FLIP_VERTICALLY);
+    }
+
+    static public FlippingDriverDecorator getFlipBothSidesDecorator(Job2dDriver job2dDriver) {
+        return new FlippingDriverDecorator(job2dDriver, FLIP_BOTH_SIDES);
     }
 }
